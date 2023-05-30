b3efb5aba46c271abc0a2f1f62d8802f8e7066bf
rmatej22 <rmatej545@gmail.com>
2023-05-30 20:14:01 +0200

    create form logic on register.component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styles: [``],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
