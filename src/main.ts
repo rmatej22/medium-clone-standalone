99b111bc1786fb1c93f7e563a13ba6a016ed0e5f
rmatej22 <rmatej545@gmail.com>
2023-05-30 19:47:40 +0200

    configure lazy loaded routes for register page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { AppRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [provideRouter(AppRoutes)],
 });
