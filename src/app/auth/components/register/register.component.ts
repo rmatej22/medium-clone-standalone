2135a0ba7836e0cd48138171474fd4a66deb228e
rmatej22 <rmatej545@gmail.com>
2023-05-30 19:34:55 +0200

    create register component

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styles: [``],
+})
+export class RegisterComponent {}
