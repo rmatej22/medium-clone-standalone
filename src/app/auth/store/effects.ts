246e5831ab7bd97f6a8e5bcad1a30078143f20fa
rmatej22 <rmatej545@gmail.com>
2023-06-04 18:57:05 +0200

    update effects.ts with login effects

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -37,10 +37,55 @@ export const registerEffect = createEffect(
   }
 );
 
-export const registerSuccessEffect = createEffect(
+export const redirectAfterRegisterEffect = createEffect(
   (actions$ = inject(Actions), router = inject(Router)) => {
     return actions$.pipe(
       ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      })
+    );
+  },
+  {
+    functional: true,
+    dispatch: false,
+  }
+);
+
+export const loginEffect = createEffect(
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService)
+  ) => {
+    return actions$.pipe(
+      ofType(authActions.login),
+      switchMap(({ request }) => {
+        return authService.login(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accessToken', currentUser.token);
+            return authActions.loginSuccess({ currentUser });
+          }),
+          catchError((errorResponse) =>
+            of(
+              authActions.loginFailure({
+                errors: errorResponse.error.errors,
+              })
+            )
+          )
+        );
+      })
+    );
+  },
+  {
+    functional: true,
+  }
+);
+
+export const redirectAfterLoginEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.loginSuccess),
       tap(() => {
         router.navigateByUrl('/');
       })
