7e32d7221610706359c1d1e85520e0133e3cbe8e
rmatej22 <rmatej545@gmail.com>
2023-06-04 19:43:24 +0200

    create and implement TopBarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './shared/components/topBar/topBar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent],
   templateUrl: './app.component.html',
   styles: [``],
 })
---
+++ src/app/shared/components/topBar/topBar.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable, combineLatest } from 'rxjs';
+import { selectCurrentUser } from 'src/app/auth/store/reducers';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+
+@Component({
+  selector: 'mc-topbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './topBar.component.html',
+  styles: [``],
+})
+export class TopBarComponent {
+  data$ = combineLatest({
+    currentUser: this.store.select(selectCurrentUser),
+  });
+
+  constructor(private store: Store) {}
+}
