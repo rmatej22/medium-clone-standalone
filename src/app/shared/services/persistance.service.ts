adc53812eae5da839607739b4b9f2b2b6a0ffba0
rmatej22 <rmatej545@gmail.com>
2023-06-04 16:45:49 +0200

    create PersistanceService

---
+++ src/app/shared/services/persistance.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PersistanceService {
+  set(key: string, data: unknown): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (error) {
+      console.error('Error saving to local storage', error);
+    }
+  }
+
+  get(key: string): unknown {
+    try {
+      const localStorageItem = localStorage.getItem(key);
+      return localStorageItem ? JSON.parse(localStorageItem) : null;
+    } catch (error) {
+      console.log('Error getting data from local storage', error);
+      return null;
+    }
+  }
+}
