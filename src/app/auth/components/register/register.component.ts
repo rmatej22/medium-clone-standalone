5ea2c8eda4932e3b3334092cf65f5a56663807dc
rmatej22 <rmatej545@gmail.com>
2023-06-04 16:17:31 +0200

    retrive backendErrors with selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -5,7 +5,10 @@ import { authActions } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
 import { AuthStateInterface } from '../../types/authState.interface';
-import { selectIsSubmitting } from '../../store/reducers';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -22,6 +25,7 @@ export class RegisterComponent {
     password: ['', Validators.required],
   });
   isSubmitting$ = this.store.select(selectIsSubmitting);
+  backendErrors$ = this.store.select(selectValidationErrors);
 
   constructor(
     private fb: FormBuilder,
