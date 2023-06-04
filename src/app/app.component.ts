ff49f499936afbb4249c9fd79614449f7667dd1b
rmatej22 <rmatej545@gmail.com>
2023-06-04 20:00:50 +0200

    dispatch getCurrentUser action from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './shared/components/topBar/topBar.component';
+import { Store } from '@ngrx/store';
+import { authActions } from './auth/store/actions';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,10 @@ import { TopBarComponent } from './shared/components/topBar/topBar.component';
   templateUrl: './app.component.html',
   styles: [``],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(authActions.getCurrentUser());
+  }
+}
