ca4341e1a4ebaa88be711ff0a30b096b0703f5e3
rmatej22 <rmatej545@gmail.com>
2023-06-18 21:45:00 +0200

    adapt routing for new globalFeed module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ export const AppRoutes: Route[] = [
   {
     path: 'register',
     loadChildren: () =>
-      import('src/app/auth/auth.routes').then((m) => m.RegisterRoutes),
+      import('src/app/auth/auth.routes').then((m) => m.registerRoutes),
   },
   {
     path: 'login',
     loadChildren: () =>
-      import('src/app/auth/auth.routes').then((m) => m.LoginRoutes),
+      import('src/app/auth/auth.routes').then((m) => m.loginRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('src/app/globalFeed/globalFeed.routes').then((m) => m.routes),
   },
 ];
--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,14 +2,14 @@ import { Route } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
-export const RegisterRoutes: Route[] = [
+export const registerRoutes: Route[] = [
   {
     path: '',
     component: RegisterComponent,
   },
 ];
 
-export const LoginRoutes: Route[] = [
+export const loginRoutes: Route[] = [
   {
     path: '',
     component: LoginComponent,
