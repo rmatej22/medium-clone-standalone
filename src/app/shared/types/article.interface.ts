4f1474c31de280936c78c36a40b94e81f9ff20d6
rmatej22 <rmatej545@gmail.com>
2023-06-18 22:02:38 +0200

    create ArticleInterface & GetFeedResponseInterface

---
+++ src/app/shared/types/article.interface.ts
@@ -0,0 +1,11 @@
+export interface ArticleInterface {
+  body: string;
+  createdAt: string;
+  description: string;
+  favorited: boolean;
+  favoritesCount: number;
+  slug: string;
+  tagList: string[];
+  title: string;
+  updatedAt: string;
+}
