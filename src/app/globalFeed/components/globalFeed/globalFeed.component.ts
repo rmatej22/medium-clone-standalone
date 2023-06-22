d9c7909ee67240939b079fc32fbd66b42002ad9c
rmatej22 <rmatej545@gmail.com>
2023-06-22 16:07:45 +0200

    create and implement banner.component

--- src/app/globalFeed/components/globalFeed/globalFeed.component.ts
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from 'src/app/shared/components/banner/banner.component';
 import { FeedComponent } from 'src/app/shared/components/feed/feed.component';
 
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [FeedComponent],
+  imports: [FeedComponent, BannerComponent],
   templateUrl: './globalFeed.component.html',
 })
 export class GlobalFeedComponent {
---
+++ src/app/shared/components/banner/banner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+})
+export class BannerComponent {}
