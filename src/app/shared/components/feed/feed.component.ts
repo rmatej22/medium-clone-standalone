be2cbb5b13bddf8b992376602565f286ba9a58a9
rmatej22 <rmatej545@gmail.com>
2023-06-22 15:32:57 +0200

    display feed atricles on feed.component

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,15 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { feedActions } from './store/actions';
+import { combineLatest } from 'rxjs';
+import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-feed',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './feed.component.html',
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = '';
+
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    feed: this.store.select(selectFeedData),
+  });
 
   constructor(private store: Store) {}
 
