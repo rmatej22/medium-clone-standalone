26b59f8997db7644c03d3fac76cca1b438ac0595
rmatej22 <rmatej545@gmail.com>
2023-06-23 11:04:45 +0200

    add logic for PopularTagsComponent and add it to GlobalFeedComponent

--- src/app/globalFeed/components/globalFeed/globalFeed.component.ts
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from 'src/app/shared/components/banner/banner.component';
 import { FeedComponent } from 'src/app/shared/components/feed/feed.component';
+import { PopularTagsComponent } from 'src/app/shared/components/popularTags/popularTags.component';
 
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [FeedComponent, BannerComponent],
+  imports: [FeedComponent, BannerComponent, PopularTagsComponent],
   templateUrl: './globalFeed.component.html',
 })
 export class GlobalFeedComponent {
--- src/app/shared/components/popularTags/popularTags.component.ts
+++ src/app/shared/components/popularTags/popularTags.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { popularTagsActions } from './store/actions';
+import { combineLatest } from 'rxjs';
+import {
+  selectError,
+  selectIsLoading,
+  selectPopularTagsData,
+} from './store/reducers';
+import { LoadingComponent } from '../loading/loading.component';
+import { ErrorMessageComponent } from '../errorMessage/errorMessage.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-popular-tags',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LoadingComponent, ErrorMessageComponent, RouterLink],
   templateUrl: './popularTags.component.html',
 })
-export class PopularTagsComponent {}
+export class PopularTagsComponent implements OnInit {
+  data$ = combineLatest({
+    popularTags: this.store.select(selectPopularTagsData),
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+  });
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(popularTagsActions.getPopularTags());
+  }
+}
