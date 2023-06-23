4171dfa67f7b6705a9933b0f35a9a3baf3ab7a93
rmatej22 <rmatej545@gmail.com>
2023-06-23 10:15:55 +0200

    create PopularTagService with getPopularTags method

---
+++ src/app/shared/components/popularTags/services/popularTag.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { PopularTagType } from 'src/app/shared/types/popularTag.type';
+import { environment } from 'src/environments/environment';
+import { GetPopularTagsResponse } from '../types/getPopularTagsResponse.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PopularTagService {
+  constructor(private http: HttpClient) {}
+
+  getPopularTags(): Observable<PopularTagType[]> {
+    const url = `${environment.apiUrl}/tags`;
+
+    return this.http
+      .get<GetPopularTagsResponse>(url)
+      .pipe(map((response) => response.tags));
+  }
+}
