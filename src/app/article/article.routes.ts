cbe1fe956ad298df62f799f3207713b3dd094899
rmatej22 <rmatej545@gmail.com>
2023-06-27 18:46:10 +0200

    provide store to article page

--- src/app/article/article.routes.ts
+++ src/app/article/article.routes.ts
@@ -1,9 +1,17 @@
 import { Route } from '@angular/router';
 import { ArticleComponent } from './components/article/article.component';
+import { provideEffects } from '@ngrx/effects';
+import * as articleEffects from './store/effects';
+import { provideState } from '@ngrx/store';
+import { articleFeatureKey, articleReducer } from './store/reducers';
 
 export const routes: Route[] = [
   {
     path: '',
     component: ArticleComponent,
+    providers: [
+      provideEffects(articleEffects),
+      provideState(articleFeatureKey, articleReducer),
+    ],
   },
 ];
