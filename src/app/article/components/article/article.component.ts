5339e08566a6cdc17c5c250df862a8be49f06501
rmatej22 <rmatej545@gmail.com>
2023-06-27 19:07:38 +0200

    create logic for rendering article on article page

--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { articleActions } from '../../store/actions';
+import { combineLatest, filter, map } from 'rxjs';
+import {
+  selectArticleData,
+  selectError,
+  selectIsLoading,
+} from '../../store/reducers';
+import { selectCurrentUser } from 'src/app/auth/store/reducers';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from 'src/app/shared/components/loading/loading.component';
+import { ErrorMessageComponent } from 'src/app/shared/components/errorMessage/errorMessage.component';
+import { TagListComponent } from 'src/app/shared/components/tagList/tagList.component';
 
 @Component({
   selector: 'mc-article',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink,
+    LoadingComponent,
+    ErrorMessageComponent,
+    TagListComponent,
+  ],
   templateUrl: './article.component.html',
 })
-export class ArticleComponent {}
+export class ArticleComponent implements OnInit {
+  slug = this.route.snapshot.paramMap.get('slug') ?? '';
+  isAuthor$ = combineLatest({
+    article: this.store.select(selectArticleData),
+    currentUser: this.store
+      .select(selectCurrentUser)
+      .pipe(
+        filter(
+          (currentUser): currentUser is CurrentUserInterface | null =>
+            currentUser !== undefined
+        )
+      ),
+  }).pipe(
+    map(({ article, currentUser }) => {
+      if (!article || !currentUser) {
+        return false;
+      }
+      return article.author.username === currentUser.username;
+    })
+  );
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    article: this.store.select(selectArticleData),
+    isAuthor: this.isAuthor$,
+  });
+
+  constructor(private store: Store, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(articleActions.getArticle({ slug: this.slug }));
+  }
+}
