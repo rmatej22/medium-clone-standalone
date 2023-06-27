ec418df0e072d373ba9b1a16b2cdc57cf56a4489
rmatej22 <rmatej545@gmail.com>
2023-06-27 18:21:20 +0200

    create article page

---
+++ src/app/article/components/article/article.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './article.component.html',
+})
+export class ArticleComponent {}
