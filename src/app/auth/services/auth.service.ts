f9d8f6e0321989d74a1b6c92175fd8a753dbeb2b
rmatej22 <rmatej545@gmail.com>
2023-06-28 16:06:55 +0200

    create updateCurrentUser method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,6 +7,7 @@ import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface
 import { environment } from 'src/environments/environment';
 import { AuthResponseInterface } from '../types/authResponse.interface';
 import { LoginRequestInterface } from '../types/loginRequest.interface';
+import { CurrentUserRequestInterface } from 'src/app/shared/types/currentUserRequest.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -35,5 +36,14 @@ export class AuthService {
     return this.http
       .post<AuthResponseInterface>(url, data)
       .pipe(map(this.getUser));
+  }
+
+  updateCurrentUser(
+    currentUserRequest: CurrentUserRequestInterface
+  ): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/user'
+    return this.http
+      .put<AuthResponseInterface>(url, currentUserRequest)
+      .pipe(map(this.getUser))
   }
 }
