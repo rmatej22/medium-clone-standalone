6c3683804b692e238da490c44a81fac5a0dd27f1
rmatej22 <rmatej545@gmail.com>
2023-06-28 13:21:58 +0200

    fix small part of code in article.service

--- src/app/article/services/article.service.ts
+++ src/app/article/services/article.service.ts
@@ -3,9 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class ArticleService {
   constructor(private http: HttpClient) {}
 
