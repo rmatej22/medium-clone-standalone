9f2f1be68c887d9253158942d46c63a6a052d9ab
rmatej22 <rmatej545@gmail.com>
2023-06-28 12:11:17 +0200

    create createArticle page and add it to app.routes

---
+++ src/app/createArticle/components/createArticle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-create-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './createArticle.component.html',
+})
+export class CreateArticleComponent {}
